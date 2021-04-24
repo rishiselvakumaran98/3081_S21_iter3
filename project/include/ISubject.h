89716ea61646c9d0830c8af8c8c684d0ce4ac33b
Rishi Selvakumaran <selva053@umn.edu>
2021-04-24 14:00:27 -0500

    Added(Data_analysis for distance): Added data anlysis csv files for the distance bwteen individual x, y, and z planes by Hamza

--- project/include/ISubject.h
+++ project/include/ISubject.h
@@ -3,7 +3,9 @@
  */
 #ifndef ISUBJECT_H_
 #define ISUBJECT_H_
-
+/**
+ * include
+ */
 #include <EntityProject/facade/delivery_system.h>
 #include "Vector3D.h"
 #include "Vector2D.h"
@@ -13,16 +15,33 @@
 namespace csci3081 {
 
 	class ISubject {
+		/**
+		 * @brief the subject class will be used as part of the observer pattern to notify the appropriate entities to changes made in the simulation.
+		 */
 		private:
+			// a list of observers that are watching the current subject.
 			std::vector<IEntityObserver*> observers_;
 
 		public:
+			/**
+			 * @brief This methods adds observers to the observers vector.
+			 * @param[In] observer_to_be_added: an observer to be added to the observers vector.
+			 */
 			void AddObserver(IEntityObserver* observer_to_be_added);
+			/**
+			 * @brief this method removes observers from the observers vector.
+			 * @param[In] the observer entity to be removed from the observers vector.
+			 */
 			void RemoveObserver(IEntityObserver* observer_to_be_removed);
+			/**
+			 * @brief this method will loop through all the observers and call the IEntityObserver OnEvent method on all of them.
+			 * @param[In] object: picojson object to be used to determine what kind of notifications to send to the entities.
+			 * @param[In] entity: an IEntity object that points to the current entity that will notify the observers.
+			 */
 			void OnEvent(const picojson::value& object, const IEntity& entity_);
 
 	}; //close class.
 
 }//close namespace 
-
-#endif
+// namespace csci3081
+#endif // ISUBJECT_H_
--- project/include/SingletonCSV.h
+++ project/include/SingletonCSV.h
@@ -1,36 +1,83 @@
+/**
+ *@file Parabolic.h
+ */
 #ifndef SINGLETONCSV_H_
 #define SINGLETONCSV_H_
+
+/**
+ * include
+ */
+
 #include <string>
 #include <fstream>
 #include <iostream>
 #include <vector>
 namespace csci3081 { 
+	/**
+	 * @brief This class provides the necessary methods to write to an csv file that will store the data collected on the drone movements.
+	 */
 	class SingletonCSV {
 		private:
+			// a SingletonCSV object pointer.
 			static SingletonCSV* csvptr;
+			/**
+			 * @brief private constructor that creats a single instance of the SingletonCSV class.
+			 */
 			SingletonCSV() {
 			}//close constructor
 
 		public:
+			/**
+			 * @brief a public constructor that destroys the single instance and the csvptr private varaible. 
+			 */
 			~SingletonCSV() {
 				delete csvptr;;
 			}
 
-
-			//this function checks if csvptr is null, if it is creates a new instance of singletonCSV and assigns it to csvptr. If it is not null, it returns csvptr.
+			/** 
+			 * @brief this function checks if csvptr is null, if it is creates a new instance of singletonCSV and assigns it to csvptr. If it is not null, it returns csvptr.
+			*/
 			static SingletonCSV* GetSingleton();
-
+			/**
+			 * @brief this method writes data to the CSV file
+			 * @param[In] filename: the name of the file to write the data to.
+			 * @param[In] num: number of bytes to write to the file.
+			*/
 			void WriteToCSV(std::string filename, float num);
 			void WriteStandardTitleToCSV(std::string filename);
 			void WritePositionToCSV(std::string filename, std::vector<float> pos, float time, float battery_level);
+			/**
+			 * @brief a helper method to write a newline character into a CSV file. This helper is used to move to the next row of data in a CSV file, ensuring proper CSV formatting of rows.
+			 * @param[In] filename: the name of the csv file to add the new line to.
+			*/
 			void helper_add_nl(std::string filename);
+			/**
+			 * @brief a helper method to add time in seconds to a CSV file given the filename. This helper is used within delivery_simulation.cc to add times from the simulation onto a .csv file for data analysis.
+			 * @param[In] filename: the name of the csv file to add the time to.
+			*/
 			void helper_add_time(std::string filename, float time);
+			/** 
+			 * @brief a method to empty the contents of a CSV file given the filename. This method is used if we need to reuse a CSV file for new data analysis or re-run a program that uses a constant csv file for data.
+			 * @param[In] filename: the name of to be cleaned.
+			 * @param[In] time: the time interval to be added to the file.
+			*/
 			void CleanFile(std::string filename);
+			/** 
+			 * @brief : a plural version of the CleanFile method that essentially uses CleanFile for every file given in a vector of .csv files. 
+			 * @param[In] filenams: a vector of filenames that contains all the files to be cleaned.
+			*/
 			void ClearFiles(std::vector<std::string> filenames);
+			/** 
+			 * @brief a generic method to add a given line to a provided vector of files. This method is overloaded to also be able to add a given time to the files.
+			 * @param[In] filenames: a vector containing all the files and the given line will be added to.
+			*/
 			void AddLineToFiles(std::vector<std::string> filenames);
+			/** 
+			 * @brief a method to add given time intervals to a provided vector of files.
+			 * @param[In] filenames: a vector of files containing all the files that the given time interval will be added to.
+			 * @param[In] t: the time interval to be added to the given filenames vector.
+			*/
 			void AddTimeToFiles(std::vector<std::string> filenames, float t);
 	}; //close class
-
-}//close namespace
-
-#endif
+}//close namespace csci3081
+#endif // SINGLETONCSV_H_
